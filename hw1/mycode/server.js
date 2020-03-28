edffc230a1ce4fdc360f4de5839cc69d4f0acd5a
George499 <gi.32@mail.ru>
2020-03-28 21:25:31 +0300

    update (not sure)

--- hw1/mycode/server.js
+++ hw1/mycode/server.js
@@ -1,19 +1,35 @@
 var express = require("express");
 const app = express();
+const LIMIT = 10;
+const DELAY = 1000;
+let connections = [];
+let date = new Date();
+let tick = 0;
 
-app.get("/", function(req, res) {
-  res.send(`<!doctype>
-  <html>
-    <head>
-    <meta charset='utf-8'>
-    <title> Основы Node </title>
-    </head>
-    <body>
-    <h1>Шаблон</h1>
-    <button onclick='alert("Нажми меня")'>Push</button>
-    </body>
-    </html>
-  `);
+app.get("/", (req, res, next) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.setHeader("Trasfer-Encoding", "chunked");
+  connections.push(res);
 });
+
+function dateToConsole(tick) {
+  console.log(`Tick number ${tick}: ${date}`);
+}
+
+setTimeout(function run() {
+  if (tick > LIMIT) {
+    connections.map(res => {
+      res.write(`tick #${tick} ${date}: End`);
+      res.end();
+    });
+    tick = 0;
+    connections = [];
+  }
+  connections.map(() => {
+    tick++;
+    dateToConsole(tick);
+  });
+  setTimeout(run, DELAY);
+}, DELAY);
 
 app.listen(3000, () => console.log("Сервер работает"));
