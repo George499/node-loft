2793ed5ff623e8e230f6d3a77d15eec25190bacc
George499 <gi.32@mail.ru>
2020-03-31 17:57:40 +0300

    дополнил исправлениями с созвона

--- hw1/mycode/myCode.js
+++ hw1/mycode/myCode.js
@@ -1,49 +1,83 @@
-const fs = require("fs");
+const fs = require("fs").promises;
+const { createReadStream, createWriteStream } = require("fs");
 const path = require("path");
 const program = require("./commander");
 const inputFolder = `${program.folder}`;
 const outputFolder = `${program.output}`;
 
-function getFiles(dir) {
-  const outputFiles = [];
-  const files = fs.readdirSync(dir);
-  files.map(i => {
+async function getFiles(dir) {
+  let outputFiles = [];
+  const files = await fs.readdir(dir);
+  for (i of files) {
     const name = path.join(dir, i);
-    if (fs.statSync(name).isDirectory()) {
-      getFiles(name, outputFiles);
+    const isThis = await fs.stat(name);
+    if (isThis.isDirectory()) {
+      const filesSubDir = await getFiles(name);
+      outputFiles = [...filesSubDir, ...outputFiles];
     } else {
       outputFiles.push(name);
     }
-  });
+  }
   return outputFiles;
 }
 
+function isAccessible(path) {
+  return fs
+    .access(path)
+    .then(() => true)
+    .catch(() => false);
+}
+function getNameFilesAndPath(item, folder) {
+  const name = path.basename(item);
+  const firstLetter = name.substr(0, 1).toLowerCase();
+  const dirName = path.join(folder, firstLetter);
+  return { name, dirName };
+}
 async function sortFiles() {
   let files = await getFiles(inputFolder);
-  if (!fs.existsSync(outputFolder)) {
-    fs.mkdirSync(outputFolder);
+
+  if (!(await isAccessible(outputFolder))) {
+    await fs.mkdir(outputFolder);
   }
-  files.map(item => {
-    const name = path.basename(item);
-    const firstLetter = name.substr(0, 1).toLowerCase();
-    const dirName = path.join(outputFolder, firstLetter);
-    if (!fs.existsSync(dirName)) {
-      fs.mkdirSync(dirName);
+  for (i of files) {
+    const nameAndPath = getNameFilesAndPath(i, outputFolder);
+    if (!(await isAccessible(nameAndPath.dirName))) {
+      await fs.mkdir(nameAndPath.dirName);
     }
-    const readable = fs.createReadStream(item);
-    const writeable = fs.createWriteStream(path.join(dirName, name));
-    readable.pipe(writeable);
+  }
+  const promiseFiles = files.map(item => {
+    return new Promise(async (resolve, reject) => {
+      const nameAndPath = getNameFilesAndPath(item, outputFolder);
+      const readable = createReadStream(item);
+      const writeable = createWriteStream(
+        path.join(nameAndPath.dirName, nameAndPath.name)
+      );
+      readable.pipe(writeable);
+      writeable.on("finish", () => {
+        resolve();
+      });
+      writeable.on("error", err => {
+        reject(err.message);
+      });
+      readable.on("error", err => {
+        reject(err.message);
+      });
+    });
   });
+
+  await Promise.all(promiseFiles.map(item => item.catch(console.log)));
 }
 
-if (program.folder) {
-  try {
-    sortFiles();
-    console.log(`Copied to ${outputFolder}`);
-  } catch (err) {
-    console.log(err);
-    process.exit(1);
+(async () => {
+  if (inputFolder) {
+    try {
+      await sortFiles();
+      console.log(`Copied to ${outputFolder}`);
+    } catch (err) {
+      console.log(err);
+      process.exit(1);
+    }
+  } else {
+    console.log(`Folder ${outputFolder} doesn't exist`);
   }
-} else {
-  console.log(`Folder ${outputFolder} doesn't exist`);
-}
+})();
